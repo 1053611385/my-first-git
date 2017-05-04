6cb7576571516aa6b8f359501f1a8f802b5ba7c6
MXH <mxh@MXHdeMacBook-Pro.local>
2017-05-04 10:32:58 +0800

    添加两个变量

--- my_first_git.java
+++ my_first_git.java
@@ -1,5 +1,8 @@
 public class my_first_git{
 	public static void main(String[] args) {
-		System.out.println("my_first_git");
+		System.out.println("my_first_git")
+
+		int a = 1;
+		int b = 2;
 	}
 }
